82bf0ab2dbbae7e76c1c467c171cbea700f8a01c
jyeung <jakeyeung@gmail.com>
2013-03-07 11:35:42 -0800

    Takes all .RData files in one directory, writes table of betas based on whether they are matrix or methylumi object.

--- Epigenetics/R_scripts/read_data.R
+++ Epigenetics/R_scripts/read_data.R
@@ -5,6 +5,8 @@
 ###############################################################################
 ###############################################################################
 
+rm(list=ls()) # Clear variables
+
 # Load required packages
 # 
 # Installing these packages on linux was a pain.
@@ -19,14 +21,38 @@ library("methylumi")
 library("lumi")
 
 
-# Load .RData files
-FileDirectory <- "/home/jyeung/Documents/Project - Kollman/R file/bin"
-setwd(FileDirectory)
-load("20120228-01 Kollman.final.RData")
+# Define directories, input contains .RData files, output is where we write to.
+InputDirectory <- "/home/jyeung/Documents/Inputs/"
+OutputDirectory <- "/home/jyeung/Documents/Outputs/"
+setwd(InputDirectory)
+
+# First, get list of .RData files in InputDirectory.
+# Second, load each .RData file.
+# Third, Check to see if it's a "matrix" or a "MethyLumiM" object
+# Fourth, if matrix, just write the table as .txt.
+# Fifth, if methylumi, get betas of methylumi object, then writes
+# 1.
+data_files <- list.files()
+vars = list()
+for (i in 1:length(data_files)){
+# 2.
+    vars_loaded <- load(data_files[i])
+    vars[i] <- mget(vars_loaded, parent.frame())
+# 3 and 4.
+    if (class(vars[[i]]) == "matrix"){
+    write.table(vars[i], file=paste(OutputDirectory, vars_loaded, "_matrix.txt", sep=""), 
+                sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+    } else if (class(vars[[i]]) == "MethyLumiM") {
+# 3 and 5
+    write.table(betas(vars[[i]]), 
+                file=paste(OutputDirectory, vars_loaded, "_betas.txt", sep=""), 
+                sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+    } else {
+    warning(paste(vars_loaded), 'does not have a matrix or a methylumi object')
+    }
+}
 
 
-# First, write expression table from .RData and put it into tab delimited file.
-# Seocnd, write experimental design into tab delimited file. 
-write.exprs(Kollman.final, file="kollman_expression.txt")
-write.table(pData(Kollman.final), file="kollman_pdata.txt", sep="\t",
-            quote=FALSE, row.names=FALSE, col.names=TRUE)
+
+
+
