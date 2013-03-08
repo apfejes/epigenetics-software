f77887dba2f2a846d52e5f0e35f1fb07f2210694
jyeung <jakeyeung@gmail.com>
2013-03-08 09:05:31 -0800

    Third commit: changed the code into a function.
    Ignores all non-methylumi objects, takes methylumi objects to get both
    beta values and design of the experiment.

--- Epigenetics/R_scripts/read_data.R
+++ Epigenetics/R_scripts/read_data.R
@@ -7,6 +7,31 @@
 
 rm(list=ls()) # Clear variables
 
+
+# Declare functions
+###############################################################################
+###############################################################################
+GetBetas <- function(x){
+    envir = environment()
+    print(envir)
+    var_loaded <- load(x, envir)
+    var <- mget(var_loaded, envir)
+    print (class(var[[1]]))
+    if (class(var[[1]]) == "MethyLumiM"){
+    write.table(betas(var[[1]]), 
+                file=paste(OutputDirectory, var_loaded, "_betas.txt", sep=""),
+                sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+    write.table(pData(var[[1]]),
+                file=paste(OutputDirectory, var_loaded, "_pData.txt", sep=""),
+                sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+    } else{
+    warning(paste(var_loaded), 'is not a MethyLumiM object')
+    }
+}
+
+# The code
+###############################################################################
+###############################################################################
 # Load required packages
 # 
 # Installing these packages on linux was a pain.
@@ -32,25 +57,8 @@ setwd(InputDirectory)
 # Fourth, if matrix, just write the table as .txt.
 # Fifth, if methylumi, get betas of methylumi object, then writes
 # 1.
-data_files <- list.files()
-vars = list()
-for (i in 1:length(data_files)){
-# 2.
-    vars_loaded <- load(data_files[i])
-    vars[i] <- mget(vars_loaded, parent.frame())
-# 3 and 4.
-    if (class(vars[[i]]) == "matrix"){
-    write.table(vars[i], file=paste(OutputDirectory, vars_loaded, "_matrix.txt", sep=""), 
-                sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
-    } else if (class(vars[[i]]) == "MethyLumiM") {
-# 3 and 5
-    write.table(betas(vars[[i]]), 
-                file=paste(OutputDirectory, vars_loaded, "_betas.txt", sep=""), 
-                sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
-    } else {
-    warning(paste(vars_loaded), 'does not have a matrix or a methylumi object')
-    }
-}
+data_files <- list.files(pattern="*.RData")
+lapply(data_files, GetBetas)
 
 
 
