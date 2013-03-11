3db0bc14691efc9c44806e82f1be08db0b806514
Anthony Fejes <afejes@cmmt.ubc.ca>
2013-03-11 13:44:23 -0700

    Adding framework for MongoDB api for the database of epigenetic data.

---
+++ Epigenetics/Database-mongo/mongoAPI.py
@@ -0,0 +1,16 @@
+'''
+Created on 2013-03-11
+
+@author: afejes
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
