dc4e0fa915ea5022f5f629ede9d3fe20e01556f3
Anthony Fejes <afejes@cmmt.ubc.ca>
2013-03-28 10:42:11 -0700

    Functioning Generic Mongo connector (API). Only performs connection, insert and disconnect.

--- Epigenetics/MongoDB/mongoUtilities/Mongo_Connector.py
+++ Epigenetics/MongoDB/mongoUtilities/Mongo_Connector.py
@@ -9,20 +9,29 @@ from pymongo.mongo_client import MongoClient
 import sys
 
 
+class MongoConnector():
 
 
-def __init__(self, collection_name):
-    '''wrapper around connect to Mongo, in order to consolidate all of the 
-    necessary connections.'''
-    try:
-        self.connection = MongoClient('kruncher.cmmt.ubc.ca', 27017)
-        self.db = self.connection.epigenetics_database
-        print('Connected to ' + self.db.name + '.')
-        self.collection = self.db[collection_name]    # Create a collection for inserting documents, user input.
-    except :
-        sys.exit("Failure to connect to database")
 
-def insert(self, dictionary):
-    self.collection.insert(dictionary)
+    def __init__(self, database_name, collection_name):
+        '''wrapper around connect to Mongo, in order to consolidate all of the 
+        necessary connections.'''
+        try:
+            self.mongo = MongoClient('kruncher.cmmt.ubc.ca', 27017)
+            self.db = self.mongo[database_name]    # Create a collection for inserting documents, user input.
+            self.collection = self.db[collection_name]
+            print self.collection.name
+            print self.collection.count()
+            self.collection.insert({"author": "Mike"})
+        except Exception, e:
+            print e
+            sys.exit("Failure to connect to database")
+
+    def insert(self, dictionary):
+        self.collection.insert(dictionary)
+
+
+    def close(self):
+        self.db.connection.disconnect()
 
 
