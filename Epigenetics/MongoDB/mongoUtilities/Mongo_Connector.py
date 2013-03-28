ee955480e1eebbd7768296a2fa574bcdd25722d0
Anthony Fejes <afejes@cmmt.ubc.ca>
2013-03-28 09:31:45 -0700

    Mongo Connector now uses an object, so multiple connections can be established at the same time.

--- Epigenetics/MongoDB/mongoUtilities/Mongo_Connector.py
+++ Epigenetics/MongoDB/mongoUtilities/Mongo_Connector.py
@@ -4,29 +4,25 @@ Created on 2013-03-27
 @author: afejes
 '''
 
-from MongoDB.mongoUtilities import ConnectToMongo
+
 from pymongo.mongo_client import MongoClient
-
-db = None
-collection = None
+import sys
 
 
-def Connect_to_Mongo(collection_name):
+
+
+def __init__(self, collection_name):
     '''wrapper around connect to Mongo, in order to consolidate all of the 
     necessary connections.'''
-
     try:
-        connection = MongoClient('kruncher.cmmt.ubc.ca', 27017)
-        db = connection.epigenetics_database
-        print('Connected to ' + db.name + '.')
-        db = ConnectToMongo.ConnectToMongo()
-        collection = db[collection_name]    # Create a collection for inserting documents, user input.
+        self.connection = MongoClient('kruncher.cmmt.ubc.ca', 27017)
+        self.db = self.connection.epigenetics_database
+        print('Connected to ' + self.db.name + '.')
+        self.collection = self.db[collection_name]    # Create a collection for inserting documents, user input.
     except :
-        print("Failure to connect to database")
-        return False
-    return True
+        sys.exit("Failure to connect to database")
 
-def insert(dictionary):
-    collection.insert(dictionary)
+def insert(self, dictionary):
+    self.collection.insert(dictionary)
 
 
