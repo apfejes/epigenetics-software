7fc7b8592e726dd52ca76333d404602af4b7beea
jyeung <jakeyeung@gmail.com>
2013-04-11 13:43:24 -0700

    Tweaked AnnotationMaker.py and ChrLocPairs.py. Commit of ProbeFinder.py and MethylAnnotation.py -> can search which probes based on chromosome and start-end location.

--- Epigenetics/Methylation_Data/ProbeFinder.py
+++ Epigenetics/Methylation_Data/ProbeFinder.py
@@ -9,21 +9,54 @@ from MongoDB.mongoUtilities import Mongo_Connector
 from annotUtilities import ChrLocPairs
 
 
+
 annot_database = 'annotation'
-annot_collection = 'methylation'
+# annot_collection = 'methylation'
+annot_collection = 'name_chr_loc'
 probe_database = 'data'
 probe_collection = 'methyl450k15'
 
 mongo_annot = Mongo_Connector.MongoConnector('kruncher.cmmt.ubc.ca', 27017, annot_database)
 mongo_data = Mongo_Connector.MongoConnector('kruncher.cmmt.ubc.ca', 27017, probe_database)
 
-pair1 = (17, 41245000)    # Chromosomme, location of BRCA1
-pair2 = (1, 149859050)    # HISTH2A2
-pair3 = (17, 7844000)    # CNTROB
+pair1 = ("1", 53468351)
+# pair2 = ("3", 37440968)
+# pair3 = ("8", -87060691)
+# pair4 = ("X", 68048840)
+# pair5 = ("3", 171757418)
 
-pairs = ChrLocPairs.ChrLocPairs(pair1, pair2, pair3)
+# pairs = ChrLocPairs.ChrLocPairs(pair1, pair2, pair3, pair4, pair5)
+pairs = ChrLocPairs.ChrLocPairs(pair1)
 
-print pairs.dic
+curs = mongo_annot.find(annot_collection, pairs.findQuery(), pairs.returnQuery())
+
+for doc in curs:
+    print doc
+
+
+'''
+chrloc_cursor = mongo_annot.find(annot_collection, pairs.findQuery(), pairs.returnQuery())
+
+list_chr = pairs.dic.keys()
+# findQuery = {'chromosome': {'$in': list_chr}}
+# returnQuery = {'probe_name': True}
+findQuery = pairs.findQuery()
+returnQuery = pairs.returnQuery()
+print findQuery
+print returnQuery
+
+curs = mongo_annot.find(annot_collection, findQuery, returnQuery)
+
+print curs.count()
+'''
+
+
+
+
+
+
+
+    
 
 
 
