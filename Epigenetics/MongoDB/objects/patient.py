93fe74f24a02ada9dc5af909ae6564766ccd1bef
Anthony Fejes <afejes@cmmt.ubc.ca>
2013-04-12 09:40:45 -0700

    Placeholders for objects that will need validation.

---
+++ Epigenetics/MongoDB/objects/patient.py
@@ -0,0 +1,18 @@
+'''
+Created on 2013-04-12
+
+@author: afejes
+'''
+
+class Patient_object(object):
+    '''
+    not currently using this object, but it will later be used to validate and
+    manage objects.
+    '''
+
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
---
+++ Epigenetics/MongoDB/objects/sample.py
@@ -0,0 +1,17 @@
+'''
+Created on 2013-04-12
+
+@author: afejes
+'''
+
+class Sample_object(object):
+    '''
+    not currently using this object, but it will later be used to validate and
+    manage objects.
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
