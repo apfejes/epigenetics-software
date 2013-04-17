d96a86dd469286e5bd0ca74db636ce7388dd8302
Anthony Fejes <afejes@cmmt.ubc.ca>
2013-04-17 09:56:47 -0700

    Including a simple tutorial example that creates a web interface to mongodb and allows reading and writing.  Will modify this to be a web interface for a basic LIMS system for the sample portion of the database.  Uses Express and node.js.

---
+++ Epigenetics/nodejs/app.js
@@ -0,0 +1,71 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
+var app = express();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+var articleProvider = new ArticleProvider('localhost', 27017);
+// Routes
+
+app.get('/', function(req, res){
+    articleProvider.findAll( function(error,docs){
+	res.render('index.jade', {title: 'Blog', articles:docs});
+    })
+});
+
+app.get('/blog/new', function(req, res) {
+    res.render('blog_new.jade', { locals: {
+        title: 'New Post'
+    }
+    });
+});
+
+app.post('/blog/new', function(req, res){
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
+app.get('/blog/:id', function(req, res) {
+    articleProvider.findById(req.params.id, function(error, article) {
+        res.render('blog_show-final.jade',{title: article.title, article:article});
+    });
+});
+
+app.post('/blog/addComment', function(req, res) {
+    articleProvider.addCommentToArticle(req.param('_id'), {
+        person: req.param('person'),
+        comment: req.param('comment'),
+        created_at: new Date()
+       } , function( error, docs) {
+           res.redirect('/blog/' + req.param('_id'))
+       });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", 27017, app.settings.env);
