f04d43957d552009a778b6653935c690dbfad18a
sperez <karatezeus21@gmail.com>
2013-05-15 15:11:39 -0700

    Can now query ensembl for exons of a gene and outputs their coordinates.

---
+++ Epigenetics/Annotations/ensemblfx.py
@@ -0,0 +1,24 @@
+'''
+Created on 2013-05-14
+
+@author: sperez
+
+Functions for querying Ensembl.
+'''
+from time import time
+from cogent.db.ensembl import Genome
+
+
+def get_Exons(gene):
+    Exons = gene.CanonicalTranscript.Exons
+    return Exons
+
+def ExonCoordinates(Exons):
+    Coordinates = []
+    t0=time()
+    for exon in Exons:
+        coords = str(exon.Location).split(":")[3].split("-")
+        coords = map(int,coords)
+        Coordinates.append(coords)
+    print "Query time: %.3f" % (time()-t0), "s."
+    return Coordinates
--- Epigenetics/Annotations/query_ensembl.py
+++ Epigenetics/Annotations/query_ensembl.py
@@ -4,55 +4,28 @@ Created on 2013-05-14
 @author: sperez
 
 largely inspired from : http://pycogent.org/examples/query_ensembl.html#query-ensembl
+and: http://pycogent.org/cookbook/accessing_databases.html
 
 '''
 
-
-
-
-import os
-#Need to declare which release of Ensemble you want to use
-Release = 67
-
-#Check if a Ensembl Account exists, otherwise, use default Ensembl UK service
-#from cogent.db.ensembl import HostAccount
-#if 'ENSEMBL_ACCOUNT' in os.environ:
-#    host, username, password = os.environ['ENSEMBL_ACCOUNT'].split()
-#    account = HostAccount(host, username, password)
-#    print "Acoount for Enseml found"
-#else:
-#    account = None
-
-account = None    
-
-
 from cogent.db.ensembl import Genome
-human = Genome(Species='human', Release=Release, account=account)
+from ensemblfx import *
 
-#This variable declares you are using the Ensembl coordinates for nucleotide positions
-#Note that Python coordinates begin at 0 and Ensembl coords begin at 1.
+#Need to declare which release of Ensemble you want to use adn the account
+Release, account = 67, None
+human = Genome(Species='human', Release=Release, account=account)
 ensembl_coord=True
 
-from time import time
-#Here is an example of a query using an ensembl id for Huntington's gene
-t0 = time()
-htt = human.getGeneByStableId(StableId='ENSG00000197386')
-print htt.Description
-print htt.Location
-print time()-t0
-#Similarly, you could have done your search using:
+#We want the coordinates of the exons of the Huntington gene
+Huntingtons = 'ENSG00000197386'
+GeneId = Huntingtons
 
+#First we grab the gene from Ensembl
+gene = human.getGeneByStableId(StableId=GeneId)
 
-#Here is where the annotation features come in handy:
-t0 = time()
-annot_brca1 = brca1.getAnnotatedSeq(feature_types='gene')
-duration = time() - t0
-print duration, annot_brca1.getAnnotationsMatching
+#Then we get the Exons
+exons = get_Exons(gene)
 
-#Getting coding regions
-t0 = time()
-cds = annot_brca1.getAnnotationsMatching('CDS')
-print time()-t0, cds, cds.getCoordinates()
-
-
+#We get the coordinates as a list of lists.
+coordinates = ExonCoordinates(exons)
 
