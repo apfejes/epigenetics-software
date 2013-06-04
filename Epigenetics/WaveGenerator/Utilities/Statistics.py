36aca0aac396ef7d512d6c2096ac059e699532af
Anthony Fejes <afejes@cmmt.ubc.ca>
2013-06-04 13:10:04 -0700

    Reformat Statistics.py ks test.

--- Epigenetics/WaveGenerator/Utilities/Statistics.py
+++ Epigenetics/WaveGenerator/Utilities/Statistics.py
@@ -17,8 +17,8 @@ class MyClass(object):
     '''
 
     def ks_test(self, mean1, sigma1, mean2, sigma2):
-        
-        #Check the pekas actually overlap...
+
+        # Check the pekas actually overlap...
         start1 = mean1 - (4 * sigma1)
         end1 = mean1 + (4 * sigma1)
         start2 = mean2 - (4 * sigma2)
@@ -28,16 +28,22 @@ class MyClass(object):
         end = min(end1, end2)
 
         if start > end:
-            return 0
+            return (0, 0)
 
-        #Calculate the point of max distance between CDFs
-        a,b,c,d = sigma1,sigma2,mean1,mean2
-        x = (a**2*d+sqrt(a**4*(-b**2)*log(b/a)+a**2*b**4*log(b/a)+a**2*b**2*c**2-2*a**2*b**2*c*d+a**2*b**2*d**2)-b**2*c)/(a**2-b**2)
-        #Find the max distance
+        # Calculate the point of max distance between CDFs
+        a, b, c, d = sigma1, sigma2, mean1, mean2
+        a2 = a * a
+        a4 = a2 * a2
+        b2 = b * b
+        b4 = b2 * b2
+        c2 = c * c
+        d2 = d * d
+        x = (a2 * d + sqrt(a4 * (-b2) * log(b / a) + a2 * b4 * log(b / a) + a2 * b2 * c2 - 2 * a2 * b2 * c * d + a2 * b2 * d2) - b2 * c) / (a2 - b2)
+        # Find the max distance
         def phi(x):
             return (1.0 + erf(x / sqrt(2.0))) / 2.0
 
-        y = fabs(phi((x-c)/a)-phi((x-d)/b))
+        y = fabs(phi((x - c) / a) - phi((x - d) / b))
         return (x, y)
 
 
