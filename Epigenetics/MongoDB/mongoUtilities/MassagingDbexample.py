889e5cf644b75bac88398fbf65db89e24f92da99
sperez <karatezeus21@gmail.com>
2013-07-03 11:18:58 -0700

    MassagingDBexample.py succesfully updates 'project' label in methylation collection.

--- Epigenetics/MongoDB/mongoUtilities/MassagingDbexample.py
+++ Epigenetics/MongoDB/mongoUtilities/MassagingDbexample.py
@@ -12,21 +12,26 @@ import os, sys
 _cur_dir = os.path.dirname(os.path.realpath(__file__))    # where the current file is
 _root_dir = os.path.dirname(_cur_dir)
 sys.path.insert(0, _root_dir + os.sep + "MongoDB" + os.sep + "mongoUtilities")
-import Mongo_Connector
+import Mongo_Connector, MongoCurious
 import MongoQuery
 
 
 m = MongoCurious.MongoCurious(database = "human_epigenetics")
 
 sample_query = m.query(collection = "samples", project = 'gecko')
+print sample_query
 docs = m.finddocs()
+print docs
 
-sample_labels_list = [doc['sample_label'] for doc in docs]
+sample_labels_list = [str(doc['sample_label']) for doc in docs]
+print len(sample_labels_list), 'samples to update'
+print sample_labels_list
 
-for doc in docs:
-    queryDict = {'sample_label':doc['sample_label']}
-    updateDict = {'project':'gecko'}
-    Mongo_Connector.update(self, 'methylation', queryDict, updateDict, multiOpt = True)
+t0 = time()
+mongo = Mongo_Connector.MongoConnector('kruncher.cmmt.ubc.ca', 27017, 'human_epigenetics')
+for sample in sample_labels_list:
+    queryDict = {'sample_label':sample}
+    updateDict = {'$set':{'project':'gecko'}}
+    mongo.update('methylation', queryDict, updateDict, multiOpt = True)
 
-m.query(collection = 'methylation', project = 'gecko')
-print len(m.finddocs())
+print "Done updating in ", (time()-t0)
