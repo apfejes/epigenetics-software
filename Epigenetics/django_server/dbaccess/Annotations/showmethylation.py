1457d576aa7bb6c010ff3f225d993d2e512d5be1
sperez <karatezeus21@gmail.com>
2013-07-19 09:17:51 -0700

    Can now query both methylation and chipseq from server.

--- Epigenetics/django_server/dbaccess/Annotations/showmethylation.py
+++ Epigenetics/django_server/dbaccess/Annotations/showmethylation.py
@@ -5,13 +5,18 @@ _root_dir = os.path.dirname(_root_dir)
 _root_dir = os.path.dirname(_root_dir)
 sys.path.insert(0, _root_dir)
 sys.path.insert(0, _root_dir + os.sep + "MongoDB" + os.sep + "mongoUtilities")
-
 from MongoDB.mongoUtilities import MongoCurious
 
+print("Connecting to database:")
 m = MongoCurious.MongoCurious(database = "human_epigenetics")
-m.query(collection = 'methylation', chromosome = "chr4", start = 3076407, end = 3245676)
-m.finddocs()
-m.collectbetas()
 
-def svgcode():
-    return m.svg(title = "Huntington gene", to_string = True, length = 300.0)
+#For Huntington gene
+#chromosome = 'chr4', start = 3076407, end = 3245676)
+
+def svgcode(chr = None, start = None, end = None):
+    print("Querying...")
+    query1 = m.query(collection = "methylation", chromosome = chr, start = start, end = end)
+    m.finddocs()
+    m.collectbetas()
+    return m.svg(to_string = True, title = "Methylation data on chr "+str(chr)+" ("+str(start)+"-"+str(end) +")", color = 'indigo', length = 300.0)
+
