0279d83c85875adc769e114554c91798441565dc
sperez <karatezeus21@gmail.com>
2013-08-23 14:40:29 -0700

    Reorganized functions used by views' functions into a file called viewtools.py

--- Epigenetics/django_server/dbaccess/views.py
+++ Epigenetics/django_server/dbaccess/views.py
@@ -6,12 +6,11 @@ Created on 2013-05-07
 from django.http import HttpResponse
 from django.utils.safestring import mark_safe
 from django.shortcuts import render
-# from django.views.generic import TemplateView
-
-from .Annotations import showmethylation, showchipseq
 
 from pymongo import Connection
 mongo = Connection('kruncher.cmmt.ubc.ca', 27017)
+
+from viewtools import panning, zoom, check, query
 
 def home_view(request):
     return render(request, 'base.jade')
@@ -66,77 +65,3 @@ def view_query_form(request):
     return render(request, 'query_form.jade', {'plot':mark_safe(svg), 'organism':o,
                                                'collection':col, 'chromosome':chrom, 'start':start,
                                                'end':end})
-
-def check(p):
-    if p['chromosome'] != 'None' and p['organism'] != 'None' and p['collection'] != 'None':
-        return True
-    else:
-        return False
-
-def zoom(zoom_symbol, start, end):
-    # Adjusts start and end value for new query
-    # ex: zoomfactor = 0.1, start = 200, end = 300
-    span = (end - start)    # span of 100bp
-    zoom_factor = zoom_factors[zoom_symbol]
-    new_span = span * zoom_factor    # span is now 10bp
-    new_start = start + span / 2 - new_span / 2    # start is now 245
-    new_end = end - span / 2 + new_span / 2    # end is now 255bp
-    print 'span', span, 'factor', zoom_factor, 'new', new_span
-    print start,end, 'are now', new_start, new_end
-    return int(new_start), int(new_end)
-
-# Dictionary of panning percentages from window that is shifted aside
-panning_percents = {'LessRight':0.6, 'MoreRight':0.9,
-               'LessLeft':-0.6, 'MoreLeft':-0.9}
-
-zoom_factors = {'ZoomIn': 0.33, 'ZoomInMore': 0.1,
-                'ZoomOut': 3, 'ZoomOutMore': 10}
-
-def panning(pan_factor, start, end):
-    # Adjusts start and end value for new query
-    # ex: pan_factor = '>>', start = 200, end = 300
-    start, end = int(start), int(end)
-    percent = panning_percents[pan_factor]    # look up percent shift in dictionary
-    print pan_factor, percent
-    shiftby = int((end - start) * percent)    # will be positive to go the right, negative to the left
-    print shiftby
-    return start + shiftby, end + shiftby
-
-def parse_form(form):
-    # Process the data in form.cleaned_data
-    organism = str(form.cleaned_data['organism'])
-    collection = str(form.cleaned_data['collection'])
-    chromosome = 'chr' + str(form.cleaned_data['chromosome'])
-    start = form.cleaned_data['start']
-    end = form.cleaned_data['end']
-#     if zoom_factor != 1:
-#         start, end = zoom(zoom_factor, start, end)
-#     if '>' or '<' in pan_factor:
-#         start, end = panning(pan_factor, start, end)
-    print "\n\n Received a form:", organism, collection, chromosome, start, end
-    parameters = {'organism':organism, 'collection': collection, 'chromosome':chromosome, 'start':start, 'end':end}
-    return  parameters
-
-def query(p):
-    length = 260.0
-    width = 80.0
-    margin = 20.0
-    if p['collection'] == 'chipseq':
-        return showchipseq.svgcode(db = p['organism'], 
-                                   chromosome = p['chromosome'], 
-                                   start = p['start'], 
-                                   end = p['end'],
-                                   length = length,
-                                   width = width,
-                                   margin = margin)
-    elif p['collection'] == 'methylation':
-        return showmethylation.svgcode(db = p['organism'], 
-                                   chromosome = p['chromosome'], 
-                                   start = p['start'], 
-                                   end = p['end'],
-                                   length = length,
-                                   width = width,
-                                   margin = margin)
-    else:
-        return HttpResponse(p['collection'] + ' is an invalid collection! Please try again...')
-
