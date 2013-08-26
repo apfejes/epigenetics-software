fac9d7c4a0364f8607237a4bf93921f800d324d8
Anthony Fejes <afejes@cmmt.ubc.ca>
2013-08-26 14:06:18 -0700

    First pass cleanup of ChipseqPlot

--- Epigenetics/Illustration/ChipseqPlot.py
+++ Epigenetics/Illustration/ChipseqPlot.py
@@ -16,8 +16,8 @@ class ChipseqPlot(object):
     '''
     classdocs
     '''
-    def __init__(self, filename, title, message, waves, start, end, 
-                 annotations, LENGTH, MARGIN, WIDTH):
+    def __init__(self, filename, title, message, waves, start, end,
+                 annotations, length, margin, width):
         '''
         Initialize this object - you need to pass it a mongo object for it to 
         operate on.
@@ -28,9 +28,9 @@ class ChipseqPlot(object):
         self.waves = waves
         self.start = start
         self.end = end
-        self.length = LENGTH    # default = 200.0
-        self.margin = MARGIN    # default = 20.0
-        self.width = WIDTH    # default = 60.0
+        self.length = length    # default = 200.0
+        self.margin = margin    # default = 20.0
+        self.width = width    # default = 60.0
         self.annotations = annotations
         self.message = message
 
@@ -52,9 +52,9 @@ class ChipseqPlot(object):
         self.offset_y = None
         self.samples_color = None
         self.axis_y_margin = None
-        
+
         if message:
-            Message = Text('[ '+message+' ]', insert = ((self.margin + self.length)/3, self.margin+ self.width/2),
+            Message = Text('[ ' + message + ' ]', insert = ((self.margin + self.length) / 3, self.margin + self.width / 2),
                     fill = "black", font_size = 12)
             self.elements.append(Message)
         else:
@@ -62,45 +62,41 @@ class ChipseqPlot(object):
 
 
     def build(self):
-        LENGTH, end, start, WIDTH, MARGIN = self.length, self.end, self.start, self. width, self.margin
-        waves = self.waves
-        offset = start
-        self.scale_x = LENGTH / (end - start)
+
+        self.scale_x = self.length / (self.end - self.start)
         tail = 1
 
         # create path objects for each peak
         heights = []
-        for (pos, height, stddev, sample_id) in waves:
+        for (pos, height, stddev, sample_id) in self.waves:
             heights.append(height)
-        maxh = max(heights)
-        self.maxh = maxh
-        scale_y = (WIDTH + MARGIN) * 0.8 / maxh
-        offset_y = (WIDTH + MARGIN) * 0.8 + MARGIN
-        self.scale_y, self.offset_y = scale_y, offset_y
+        self.maxh = max(heights)
+        self.scale_y = (self.width + self.margin) * 0.8 / self.maxh
+        self.offset_y = (self.width + self.margin) * 0.8 + self.margin
 
         sample_count = 0
         samples_color = {}
-        for (pos, height, stddev, sample_id) in waves:
+        for (pos, height, stddev, sample_id) in self.waves:
             print "    Peak", pos, height, stddev
-            X, Y = self.makegaussian(start, end, MARGIN, LENGTH, pos, tail, offset, float(height), stddev)
-            X = [round((x - offset + pos) * self.scale_x, 2) + 20 for x in X]
+            X, Y = self.makegaussian(self.start, self.end, pos, tail, self.start, float(height), stddev)
+            X = [round((x - self.start + pos) * self.scale_x, 2) + 20 for x in X]
             for x in X:
-                if x < (MARGIN + 1):
-                    X.insert(0, MARGIN)
+                if x < (self.margin + 1):
+                    X.insert(0, self.margin)
                     Y.insert(0, tail)
                     break
-                if x > (MARGIN + LENGTH - 1):
-                    X.append(MARGIN + LENGTH)
+                if x > (self.margin + self.length - 1):
+                    X.append(self.margin + self.length)
                     Y.append(tail)
                     break
             # Scale Y and inverse the coordinates
-            Y = [round(y * scale_y, 2) for y in Y]
-            Y = [(offset_y - y) for y in Y]
+            Y = [round(y * self.scale_y, 2) for y in Y]
+            Y = [(self.offset_y - y) for y in Y]
             # d is the list of coordinates with commands such as
             # M for "move to' to initiate curve and S for smooth curve
             d = "M" + str(X[0]) + "," + str(Y[0]) + " " + str(X[1]) + "," + str(Y[1])
             for i in range(2, len(X)):
-                d = d + (" " + str(X[i]) + "," + str(Y[i]))
+                d += (" " + str(X[i]) + "," + str(Y[i]))
 
             if sample_id not in samples_color :
                 sample_count += 1
@@ -116,11 +112,14 @@ class ChipseqPlot(object):
 
 
     def save(self):
+        '''TODO: add docstring'''
         for element in self.elements:
             self.plot.add(element)
+        self.elements = None    # may want to remove this, if we ever want to do fancy stuff with the elements.
         self.plot.save()
 
     def to_string(self):
+        '''TODO: add docstring'''
         for element in self.elements:
             self.plot.add(element)
         z = self.plot.tostring()
@@ -128,30 +127,31 @@ class ChipseqPlot(object):
         return z
 
     def get_elements(self):
+        '''TODO: add docstring'''
         self.add_sample_labels(self.margin * 3.2 + self.length)
         z = self.elements
         self.elements = None
         return z
 
-    def add_data(self, elements = None):
-        elements_to_add = elements
-        if not isinstance(elements_to_add, list):
+    def add_data(self, elements):
+        '''TODO: add docstring'''
+        if not isinstance(elements, list):
             raise ValueError("Data to add to plot should be stored in a list, not a {}".format(type(elements)))
-        for element in elements_to_add:
+        for element in elements:
             self.plot.add(element)
         print "% i svg elements have been added to the current svg object." % len(elements)
 
-    def add_legends(self,get_tss, get_cpg):
+    def add_legends(self, get_tss, get_cpg):
         ''' Add title, axis, tic marks and labels '''
         if self.title is None:
             self.title = "Chipseq Peaks"
-        Title = Text(self.title, insert = (self.margin/3, self.margin/3),
+        Title = Text(self.title, insert = (self.margin / 3, self.margin / 3),
                 fill = "midnightblue", font_size = bigfont)
         self.elements.append(Title)
 
         for axis in get_axis(self.start, self.end, self.length, self.margin, self.width, self.axis_x_margin, self.axis_y_margin):
             self.elements.append(axis)
-            
+
         if self.message is '':
             self.add_xtics()
             self.add_ytics()
@@ -165,10 +165,11 @@ class ChipseqPlot(object):
 
 
     def add_sample_labels(self, x_position):
+        '''TODO: add docstring'''
         if len(self.samples_color) > 20:
-            fontsize = str(float(medfont)-0.5)
+            fontsize = str(float(medfont) - 0.5)
         elif len(self.samples_color) < 5:
-            fontsize = str(float(medfont)+0.5)
+            fontsize = str(float(medfont) + 0.5)
         else: fontsize = medfont
 
         spacing = 0.1
@@ -181,12 +182,10 @@ class ChipseqPlot(object):
             self.elements.append(label)
         return None
 
-    def makegaussian(self, start,
-                     end, MARGIN, LENGTH,
-                     pos, tail, offset,
-                     height, stddev):
-        '''To do: remove LENGTH and MARGIN, or use them'''
-        endpts = int((sqrt((-2) * stddev * stddev * log(tail / height))))
+    @staticmethod
+    def makegaussian(start, end, pos, tail, offset, height, stddev):
+        '''TODO: Add docstring'''
+        endpts = int((sqrt((-2) * stddev * stddev * log(float(tail) / height))))
         spacing = 64
         n_points = 0
         while n_points < 25 and spacing >= 2:
@@ -199,7 +198,8 @@ class ChipseqPlot(object):
                 X.append(float(i))
             n_points = len(X)
             spacing /= 2
-        if (endpts) not in X: X.append(endpts)
+        if (endpts) not in X:
+            X.append(endpts)
         X.sort()
         X = [float(x) for x in X if 0 <= (x + pos - offset) < (end - start)]
         stddev = float(stddev)
@@ -207,52 +207,49 @@ class ChipseqPlot(object):
         return X, Y
 
     def add_xtics(self):
-        end, start, WIDTH, MARGIN = self.end, self.start, self. width, self.margin
-        offset = start
-        scale_x = self.scale_x
+        '''TODO: add docstring'''
+        # offset is self.start
         scale_tics = 1
 
-        while((scale_tics * 10) < end - start):
+        while((scale_tics * 10) < self.end - self.start):
             scale_tics *= 10
-        xtics = [i for i in range(start, end + 1) if i % (scale_tics) == 0]
+        xtics = [i for i in range(self.start, self.end + 1) if i % (scale_tics) == 0]
         while len(xtics) < 4:
             scale_tics /= 2
-            xtics += [i for i in range(start, end + 1) if i % (scale_tics) == 0 and i not in xtics]
+            xtics += [i for i in range(self.start, self.end + 1) if i % (scale_tics) == 0 and i not in xtics]
         xtics.sort()
-        spacing = fabs((MARGIN + (xtics[1] - offset) * scale_x) - (MARGIN + (xtics[0] - offset) * scale_x)) / 4
+        spacing = fabs((self.margin + (xtics[1] - self.start) * self.scale_x) - (self.margin + (xtics[0] - self.start) * self.scale_x)) / 4
         for tic in xtics:
-            tic_x = (MARGIN + (tic - offset) * scale_x)
-            tic_y = WIDTH + MARGIN * 2
+            tic_x = (self.margin + (tic - self.start) * self.scale_x)
+            tic_y = self.width + self.margin * 2
             ticmarker = (Text(str(tic), insert = (tic_x, tic_y), fill = "midnightblue", font_size = smallfont))
-            ticline = Rect(insert = (tic_x, WIDTH + MARGIN * 2 - 5 - 1), size = (0.1, 2), fill = "midnightblue")
+            ticline = Rect(insert = (tic_x, self.margin + self.margin * 2 - 5 - 1), size = (0.1, 2), fill = "midnightblue")
             for i in range (1, 4):
-                if tic_x - spacing * i > MARGIN - 5:
-                    ticline2 = Rect(insert = (tic_x - spacing * i, WIDTH + MARGIN * 2 - 5 - 1), size = (0.1, 1), fill = "midnightblue")
+                if tic_x - spacing * i > self.margin - 5:
+                    ticline2 = Rect(insert = (tic_x - spacing * i, self.width + self.margin * 2 - 5 - 1), size = (0.1, 1), fill = "midnightblue")
                     self.elements.append(ticline2)
             self.elements.append(ticline)
             self.elements.append(ticmarker)
 
     def add_ytics(self):
-        maxh, MARGIN = self.maxh, self.margin
-        scale_y, offset_y = self.scale_y, self.offset_y
-
+        '''TODO: add docstring'''
         scale_tics = 64
-        ytics = [i for i in range(0, int(maxh) + 1, scale_tics)]
+        ytics = [i for i in range(0, int(self.maxh) + 1, scale_tics)]
         while len(ytics) < 4:
             scale_tics /= 2
-            ytics += [i for i in range(0, int(maxh) + 1, scale_tics) if i not in ytics]
-        ytics = [round(offset_y - y * scale_y, 3) for y in ytics]
+            ytics += [i for i in range(0, int(self.maxh) + 1, scale_tics) if i not in ytics]
+        ytics = [round(self.offset_y - y * self.scale_y, 3) for y in ytics]
         spacing = (ytics[0] - ytics[1]) / 2
         for tic in ytics:
-            ticline = Rect(insert = (MARGIN - 5 - 1, tic), size = (2, 0.1), fill = "midnightblue")
-            ticline2 = Rect(insert = (MARGIN - 5, tic + spacing), size = (1, 0.1), fill = "midnightblue")
-            tic_x = MARGIN - 13
+            ticline = Rect(insert = (self.margin - 5 - 1, tic), size = (2, 0.1), fill = "midnightblue")
+            ticline2 = Rect(insert = (self.margin - 5, tic + spacing), size = (1, 0.1), fill = "midnightblue")
+            tic_x = self.margin - 13
             tic_y = tic + 1
-            label = str(int(round((offset_y - tic) / scale_y)))
+            label = str(int(round((self.offset_y - tic) / self.scale_y)))
             if len(label) == 1:
-                tic_x = tic_x + 3
+                tic_x += 3
             if len(label) == 2:
-                tic_x = tic_x + 2
+                tic_x += 2
             ticmarker = (Text(label, insert = (tic_x, tic_y), fill = "midnightblue", font_size = smallfont))
             self.elements.append(ticline)
             self.elements.append(ticline2)
