554589a9f3a6043c0f898083ac9dfd4123be7538
Anthony Fejes <afejes@cmmt.ubc.ca>
2013-09-03 14:49:02 -0700

    Formatting Color_palette

--- Epigenetics/Illustration/Color_Palette.py
+++ Epigenetics/Illustration/Color_Palette.py
@@ -9,47 +9,47 @@ class ColorPalette(object):
     Stores organization of color scheme for each part of plot.
     '''
     def __init__(self):
-        
+
         self.type_count = 0
-        
-        self.counter = {} #keeps track of 
-        
+
+        self.counter = {}    # keeps track of
+
         self.types_color = {}
         self.samples_color = {}
-        
+
         self.colors = {}
-    
+
     def Colors(self):
-        self.colors['blue']=['blue','cornflowerblue','darkblue','deepskyblue','darkturquoise',
-                       'midnightblue','navy','dodgerblue', 'lightblue', 'lightskyblue','cadetblue','teal',
-                       'paleturquoise', 'aquamarine', 'azure', 'aqua', 'lightsteelblue','powderblue']
-        self.colors['green']=['lightseagreen','limegreen','lawngreen', 'olivedrab', 'chartreuse','mediumturquoise', 
-                               'mediumspringgreen','forestgreen', 'seagreen','palegreen', 'olive', 'darkcyan',
-                               'yellowgreen', 'darkolivegreen','darkgreen', 'darkseagreen', 'lime']
-        self.colors['red']=['orangered', 'tomato','orange', 'gold', 'firebrick', 'sandybrown', 
+        self.colors['blue'] = ['blue', 'cornflowerblue', 'darkblue', 'deepskyblue', 'darkturquoise',
+                       'midnightblue', 'navy', 'dodgerblue', 'lightblue', 'lightskyblue', 'cadetblue', 'teal',
+                       'paleturquoise', 'aquamarine', 'azure', 'aqua', 'lightsteelblue', 'powderblue']
+        self.colors['green'] = ['lightseagreen', 'limegreen', 'lawngreen', 'olivedrab', 'chartreuse', 'mediumturquoise',
+                               'mediumspringgreen', 'forestgreen', 'seagreen', 'palegreen', 'olive', 'darkcyan',
+                               'yellowgreen', 'darkolivegreen', 'darkgreen', 'darkseagreen', 'lime']
+        self.colors['red'] = ['orangered', 'tomato', 'orange', 'gold', 'firebrick', 'sandybrown',
                              'lightcoral', 'crimson', 'coral', 'darkred', 'indianred', 'maroon']
-        self.colors['purple']=['darkslategrey','orchid', 'magenta', 'purple', 'blueviolet','darkviolet', 
+        self.colors['purple'] = ['darkslategrey', 'orchid', 'magenta', 'purple', 'blueviolet', 'darkviolet',
                            'pink', 'mediumslateblue', 'lightpink', 'deeppink', 'indigo', 'lavenderblush',
                            'violet', 'mediumorchid', 'mediumpurple', 'thistle', 'darkmagenta', 'plum']
-        self.color_wheel = {1:'blue', 2:'red', 4:'purple', 3:'green'} 
+        self.color_wheel = {1:'blue', 2:'red', 4:'purple', 3:'green'}
 
         return self.colors, self.color_wheel
-    
+
     def sorter(self, sample_type, sample_id):
-        #print '\n', self.counter, sample_type, sample_id
+        # print '\n', self.counter, sample_type, sample_id
         if sample_type not in self.types_color:
             self.type_count += 1
             if self.type_count >= len(self.color_wheel.keys()):
                 self.type_count = 1
-            self.types_color[sample_type] = self.color_wheel[self.type_count] #Assign a hue to sample_type
+            self.types_color[sample_type] = self.color_wheel[self.type_count]    # Assign a hue to sample_type
             self.counter[sample_type] = 0
-            #print 'types:', self.type_count, sample_type, self.color_wheel[self.type_count]
+            # print 'types:', self.type_count, sample_type, self.color_wheel[self.type_count]
         if sample_id not in self.samples_color:
             self.samples_color[sample_id] = self.colors[self.types_color[sample_type]][self.counter[sample_type]]
-            self.counter[sample_type]+=1
+            self.counter[sample_type] += 1
             if self.counter[sample_type] >= len(self.colors[self.types_color[sample_type]]):
-                self.counter[sample_type]=0
-        #print len(self.colors[self.types_color[sample_type]]), self.colors[self.types_color[sample_type]]
+                self.counter[sample_type] = 0
+        # print len(self.colors[self.types_color[sample_type]]), self.colors[self.types_color[sample_type]]
         if self.counter[sample_type] > len(self.colors[self.types_color[sample_type]]):
             print self.counter
             print sample_type, sample_id
@@ -59,7 +59,7 @@ class ColorPalette(object):
             raise ValueError("Ran out of colours!")
         sample_color = self.samples_color[sample_id]
         type_color = self.types_color[sample_type]
-        #print type_color, sample_color
+        # print type_color, sample_color
         return type_color, sample_color
 
     def colors_dict(self):
