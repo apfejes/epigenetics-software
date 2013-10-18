f55d0062c09bc0730617c45625af78f71e523ee4
scottdbrown <scottbrown@telus.net>
2013-10-18 11:13:43 -0700

    bugfix: run() needed to accept two parameters

--- Epigenetics/WaveGenerator/Utilities/MapDecomposingThread.py
+++ Epigenetics/WaveGenerator/Utilities/MapDecomposingThread.py
@@ -254,7 +254,7 @@ class MapDecomposer(multiprocessing.Process):
         cProfile.runctx("self.run(args)", globals(), locals())
         self.print_queue.put("finished profiling")
 
-    def run(self):    # , *args):
+    def run(self, *args):
         '''process the maps, while the thread is running.'''
         while True:
             try:
