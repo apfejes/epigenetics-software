4be82cef80728c4cb6078f67fc90e1e4566d54fd
scottdbrown <scottbrown@telus.net>
2013-11-18 09:17:37 -0800

    Bugfix: was incorrectly comparing positions Bugfix: Thought wave was after bin if position after, but on next chromosome

--- Epigenetics/WaveGenerator/analysis_compare.py
+++ Epigenetics/WaveGenerator/analysis_compare.py
@@ -43,6 +43,9 @@ def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
             wave["used"] = False
             waves.append(wave)
     f.close()
+    waves[:] = [x for x in waves if x['height'] > thresh]
+
+    waves.sort(key = lambda x: (x['chr'], x['pos']))    # list sorted by position
 
     bed = []
     f = open(bedfile, 'r')
@@ -51,14 +54,58 @@ def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
         region = {}
         region["chr"] = "chr" + chromosomes[int(a[0].replace("chr", ""))]
         # print "chromosome is: ", region["chr"]
-        region["start"] = a[1]
-        region["end"] = a[2]
-        region["intensity"] = a[3].rstrip()
+        region["start"] = int(a[1])
+        region["end"] = int(a[2])
+        region["intensity"] = float(a[3].rstrip())
         bed.append(region)
+
+    bed.sort(key = lambda x: (x['chr'], x['start']))
 
     maxperbin = 0
 
     print "Now finding peaks in each region..."
+    w = 0
+    b = 0
+    count = 0
+    height = 0
+
+    while b < len(bed) and w < len(waves):
+        if(waves[w]['chr'] == bed[b]['chr'] and waves[w]['pos'] >= bed[b]['start'] and waves[w]['pos'] <= bed[b]['end']):
+            # print "found a wave in bin ", b
+            count += 1
+            if count > maxperbin:
+                maxperbin = count
+            height += waves[w]['height']
+            waves[w]['used'] = True
+            w += 1
+        elif waves[w]['chr'] > bed[b]['chr'] or (waves[w]['pos'] > bed[b]['end'] and waves[w]['chr'] == bed[b]['chr']):
+            # wave is past bin, move to next bin
+            # write to file
+            # print "b: %i, w: %i, wave is AFTER, start: %s, end: %s, pos: %s, %s %s" % (b, w, bed[b]['start'], bed[b]['end'], waves[w]['pos'], bed[b]['chr'], waves[w]['chr'])
+            print_queue.put(bed[b]['chr'] + "\t" + str(bed[b]['start']) + "\t" + str(bed[b]['end']) + "\t" + str(bed[b]["intensity"]) + "\t" + str(count) + "\t" + str(height))
+            b += 1
+            count = 0
+            height = 0
+        else:
+            # wave is before bin
+            # print "b: %i, w: %i, wave is BEFORE, start: %s, end: %s, pos: %s, %s %s" % (b, w, bed[b]['start'], bed[b]['end'], waves[w]['pos'], bed[b]['chr'], waves[w]['chr'])
+            w += 1
+
+    while b < len(bed):
+        # if ended because got to end of waves
+        # need to write statistics for remaining bins
+        # print "filling in remaining bins with 0s"
+        start = bed[b]["start"]
+        end = bed[b]["end"]
+        chrom = bed[b]["chr"]
+
+        count = 0
+        height = 0
+
+        print_queue.put(str(chrom) + "\t" + str(start) + "\t" + str(end) + "\t" + str(bed[b]["intensity"]) + "\t" + str(count) + "\t" + str(height))
+        b += 1
+
+    '''
     for i in bed:
         start = int(i["start"])
         end = int(i["end"])
@@ -67,7 +114,11 @@ def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
         count = 0
         height = 0
         for j in waves:
-            if not j["used"] and j["height"] > thresh and j["chr"] == chrom and j["pos"] >= start and j["pos"] <= end:
+            # print "wave pos: ", j['pos']
+            if j['pos'] > end:
+                # print "BREAKING!"
+                break
+            if not j["used"] and j["chr"] == chrom and j["pos"] >= start and j["pos"] <= end:
                 # print "Found a peak."
                 count += 1
                 if count > maxperbin:
@@ -76,7 +127,7 @@ def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
                 j["used"] = True
         # print "on line: ", i
         print_queue.put(str(chrom) + "\t" + str(start) + "\t" + str(end) + "\t" + str(i["intensity"]) + "\t" + str(count) + "\t" + str(height))
-
+    '''
 
     # end printing
     if print_thread is None or not print_thread.is_alive():
@@ -86,16 +137,17 @@ def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
             print "waiting on print_queue to empty", print_queue.qsize()
             time.sleep(1)
         print_thread.END_PROCESSES = True
-        while print_thread.IS_CLOSED == False:
-            print "waiting for print_thread to close"
-            time.sleep(1)
+        print_thread.f.close()
+        # while not print_thread.is_closed():
+        #    print "waiting for print_thread to close ", print_queue.qsize(), " ", print_thread.is_closed(), " ", print_thread.END_PROCESSES
+        #    time.sleep(1)
 
     # quick summary statistics
     unassigned = 0
     for i in waves:
         if not i["used"]:
             unassigned += 1
-    print "%s waves were unassigned to a BED bin." % unassigned
+    # print "%s waves were unassigned to a BED bin." % unassigned
 
     print "Max per bin is: ", maxperbin
 
@@ -106,10 +158,10 @@ def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
     for line in f:
         ln += 1
         a = line.split("\t")
-        print "on line ", ln, " \n line is: ", line, " \nand value is: ", a[4]
+        # print "on line ", ln, " \n line is: ", line, " \nand value is: ", a[4]
         counts[int(a[4])] += 1
     f.close()
-
+    print "%s waves were unassigned to a BED bin." % unassigned
     for i in range(0, maxperbin + 1):
         print "# of bins with %s peaks: %s" % (i, counts[i])
 
@@ -117,6 +169,6 @@ if __name__ == "__main__":
 
     thresh = raw_input("Enter the threshold for peak height: ")
     thresh = float(thresh)
-    compare_BED_and_waves("/home/sbrown/Brainstorm/09-08-11_Z_yaf9.bed", "/home/sbrown/Phoebe_CEL/WAVES/09-08-11-Z_yaf9_TRP_IP.normalized.waves", "/home/sbrown/temp", thresh)
+    compare_BED_and_waves("/home/sbrown/Brainstorm/09-08-11_Z_yaf9.bed", "/home/sbrown/Phoebe_CEL/WAVES/partial_files/09-08-11-Z_yaf9_TRP_IP.normalized.waves", "/home/sbrown/temp", thresh)
     # body of main goes here
     # pass output/path to compare_BED_and_waves
