99148ed6be183c8da80583e1330006b91618aef0
scottdbrown <scottbrown@telus.net>
2013-11-21 12:06:04 -0800

    feature: now detect height threshold automatically feature: outputs summary files

--- Epigenetics/WaveGenerator/analysis_compare.py
+++ Epigenetics/WaveGenerator/analysis_compare.py
@@ -7,6 +7,7 @@ Created on 2013-11-12
 import sys
 import time
 import os
+from scipy import stats as scipystats
 
 _cur_dir = os.path.dirname(os.path.realpath(__file__))    # where the current file is
 _root_dir = os.path.dirname(_cur_dir)
@@ -15,17 +16,21 @@ sys.path.insert(0, _cur_dir + os.sep + "Utilities")
 import multiprocessing
 import PrintThread
 
+while ("WaveGenerator" in _root_dir):
+    _root_dir = os.path.dirname(_root_dir)
+sys.path.insert(0, _root_dir)
+sys.path.insert(0, _cur_dir)
+sys.path.insert(0, _root_dir + os.sep + "CommonUtils")
+sys.path.insert(0, _root_dir + os.sep + "WaveGenerator" + os.sep + "Utilities")
+import StringUtils
 
 
-def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
+
+def compare_BED_and_waves(bedfile, wavesfile, output):
     # body of function goes here
 
     chromosomes = {1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX", 10:"X", 11:"XI", 12:"XII", 13:"XIII", 14:"XIV", 15:"XV", 16:"XVI"}
 
-
-    print_queue = multiprocessing.Queue()
-    # launch thread to read and process the print queue
-    print_thread = PrintThread.StringWriter(print_queue, output, "comparison.txt", True, True)
     waves = []
 
     print "Reading files..."
@@ -43,6 +48,34 @@ def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
             wave["used"] = False
             waves.append(wave)
     f.close()
+
+    # print "\nNow determining background levels for height of peaks"
+    bins = 70    # based on max peak height of 7
+    counts = [0] * bins
+    thresh = [0] * bins
+    for i in range(0, bins):
+        thresh[i] = (i + 1) * 0.1
+    for i in waves:
+        # check all heights to determine background levels
+        counts[int(i['height'] / 0.1)] += 1    # increment count where height1 is in bin of size 0.1
+    # print "counts are: ", counts
+    x = []
+    y = []
+    for i in range(10, 15):    # (0 to 9 correspond to heights 0 to 0.9, do not have)
+        x.append(thresh[i])
+        y.append(counts[i])
+    # print "x is ", x
+    # print "y is ", y
+    slr = scipystats.linregress(x, y)
+    slope = slr[0]
+    intercept = slr[1]
+    # print "slope: %s and intercept: %s for background peak height" % (slope, intercept)
+    # find x-intercept, threshold for noise-signal
+    xint = abs(intercept / slope)
+    # print "height threshold between noise and signal is ", xint
+    thresh = round(xint, 2)
+
+    # remove waves that don't meet threshold
     waves[:] = [x for x in waves if x['height'] > thresh]
 
     waves.sort(key = lambda x: (x['chr'], x['pos']))    # list sorted by position
@@ -62,6 +95,10 @@ def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
     bed.sort(key = lambda x: (x['chr'], x['start']))
 
     maxperbin = 0
+
+    print_queue = multiprocessing.Queue()
+    # launch thread to read and process the print queue
+    print_thread = PrintThread.StringWriter(print_queue, output, StringUtils.rreplace(os.path.basename(bedfile), ".bed", "_" + str(thresh) + "_summary.txt", 1), True, True)
 
     print "Now finding peaks in each region..."
     w = 0
@@ -105,30 +142,6 @@ def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
         print_queue.put(str(chrom) + "\t" + str(start) + "\t" + str(end) + "\t" + str(bed[b]["intensity"]) + "\t" + str(count) + "\t" + str(height))
         b += 1
 
-    '''
-    for i in bed:
-        start = int(i["start"])
-        end = int(i["end"])
-        chrom = str(i["chr"])
-        # print "Checking region %s\t%i\t%i" % (chrom, start, end)
-        count = 0
-        height = 0
-        for j in waves:
-            # print "wave pos: ", j['pos']
-            if j['pos'] > end:
-                # print "BREAKING!"
-                break
-            if not j["used"] and j["chr"] == chrom and j["pos"] >= start and j["pos"] <= end:
-                # print "Found a peak."
-                count += 1
-                if count > maxperbin:
-                    maxperbin = count
-                height += j["height"]
-                j["used"] = True
-        # print "on line: ", i
-        print_queue.put(str(chrom) + "\t" + str(start) + "\t" + str(end) + "\t" + str(i["intensity"]) + "\t" + str(count) + "\t" + str(height))
-    '''
-
     # end printing
     if print_thread is None or not print_thread.is_alive():
         pass
@@ -142,17 +155,27 @@ def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
         #    print "waiting for print_thread to close ", print_queue.qsize(), " ", print_thread.is_closed(), " ", print_thread.END_PROCESSES
         #    time.sleep(1)
 
+
+    print_queue = multiprocessing.Queue()
+    # launch thread to read and process the print queue
+    print_thread = PrintThread.StringWriter(print_queue, output, StringUtils.rreplace(os.path.basename(bedfile), ".bed", "_" + str(thresh) + "_counts.txt", 1), True, True)
+
+
     # quick summary statistics
+    print_queue.put("nwaves\tnbins\tavgBinSize")
     unassigned = 0
     for i in waves:
         if not i["used"]:
             unassigned += 1
     # print "%s waves were unassigned to a BED bin." % unassigned
 
-    print "Max per bin is: ", maxperbin
+    print "Max per bin is:", maxperbin
+    print "Height threshold applied to waves:", thresh
 
     counts = [0] * (maxperbin + 1)
-    f = open(output + "/comparison.txt", 'r', 0)
+    # sizes = [[]] * (maxperbin + 1)
+    sizes = [[] for x in range(0, maxperbin + 1)]
+    f = open(output + StringUtils.rreplace(os.path.basename(bedfile), ".bed", "_" + str(thresh) + "_summary.txt", 1), 'r', 0)
 
     ln = 0
     for line in f:
@@ -160,10 +183,32 @@ def compare_BED_and_waves(bedfile, wavesfile, output, thresh):
         a = line.split("\t")
         # print "on line ", ln, " \n line is: ", line, " \nand value is: ", a[4]
         counts[int(a[4])] += 1
+        sizes[int(a[4])].append(int(a[2]) - int(a[1]))
     f.close()
     print "%s waves were unassigned to a BED bin." % unassigned
+    print_queue.put(str(unassigned) + "\t0\t0")
     for i in range(0, maxperbin + 1):
-        print "# of bins with %s waves: \t%s" % (i, counts[i])
+        tot = 0
+        # print len(sizes[i])
+        for j in range(0, len(sizes[i])):
+            tot += sizes[i][j]
+            # print "size is: ", sizes[i][j]
+        if len(sizes[i]) != 0:
+            avg = float(tot) / len(sizes[i])
+        else:
+            avg = 0
+        print "# of bins with %s waves: %s, and average size is: %s" % (i, counts[i], avg)
+        print_queue.put(str(i) + "\t" + str(counts[i]) + "\t" + str(avg))
+
+    # end printing
+    if print_thread is None or not print_thread.is_alive():
+        pass
+    else:
+        while print_queue.qsize() > 0:
+            print "waiting on print_queue to empty", print_queue.qsize()
+            time.sleep(1)
+        print_thread.END_PROCESSES = True
+        print_thread.f.close()
 
 if __name__ == "__main__":
     if len(sys.argv) <= 3:
@@ -173,7 +218,5 @@ if __name__ == "__main__":
     bed = sys.argv[1]
     wave = sys.argv[2]
     out = sys.argv[3]
-    thresh = raw_input("Enter the threshold for peak height: ")
-    thresh = float(thresh)
-    compare_BED_and_waves(bed, wave, out, thresh)
+    compare_BED_and_waves(bed, wave, out)
 
