17bf7beeb5bcd3a3d921ab17dd96654f9b7f820d
scottdbrown <scottbrown@telus.net>
2013-11-21 12:06:25 -0800

    bugfix: does not double count peaks when checking height threshold

--- Epigenetics/WaveGenerator/Compare_wave_sets_nobg_2way.py
+++ Epigenetics/WaveGenerator/Compare_wave_sets_nobg_2way.py
@@ -77,7 +77,7 @@ class WavePair():
     def set_b(self, value):
         '''update bin number for pair'''
         self.b = value
-    
+
     def set_fc(self, value):
         '''update fold change for pair'''
         self.fc = value
@@ -98,7 +98,7 @@ def run(output, db):
 
     print_queue = multiprocessing.Queue()
     # launch thread to read and process the print queue
-    print_thread = PrintThread.StringWriter(print_queue, output, "waves_nobg_2way.txt", True, True)
+    print_thread = PrintThread.StringWriter(print_queue, output, "waves_comparison_allwaves.txt", True, True)
 
     # ask user for name of sample
     if db == "yeast_epigenetics":
@@ -130,17 +130,7 @@ def run(output, db):
             sys.exit()
         samples.append(s[int(user_input)])
     print samples
-    # ask user for name of control()
 
-    # process s and samples.
-    # no need to print samples out again...
-    # s = [len(results)]
-    # c = 1
-    # for r in results:
-    #    s.append(r)
-    #    print ("%i. %s") % (c, r)
-    #    c += 1
-    # print "here are the names of available samples: which one do you want to use as a control?"
     print "which one do you want to use as a control?"
     user_input = raw_input("enter the number(s). If using more than one, separate by a comma: ")
     controls = []
@@ -160,140 +150,20 @@ def run(output, db):
         controls.append(s[int(user_input)])
     print controls
 
+    print "\n"
+    user_ks_fdr = float(raw_input("What False Discovery Rate is acceptable for pairing peaks: "))
+    print "\n"
+    user_fc = float(raw_input("What minimum fold change in peak heights are you interested in: "))
+
     chromosomes = util.get_chromosome_names()
     # #TODO: FOLLOWING FOR DEBUGGING ONLY!
     # chromosomes = ["chr1", "chr2"]
 
-    # id_s = map(util.get_sample_id_from_name, samples)
-    # id_r = map(util.get_sample_id_from_name, controls)
+
     id_s = [util.get_sample_id_from_name(s, db) for s in samples]
     id_r = [util.get_sample_id_from_name(c, db) for c in controls]
 
-    
-    
-    
-    # no need to do normalization for now...
-    '''
-    
-    
-    # print "sample ids", id_s    #these are the ObjectIds
-    # print "control ids", id_r
-    x = []
-    y = []
-    all_peaks = []
-    for chromosome in chromosomes:    # for each chromosome
-        print "chromosome %s" % chromosome
-        waves1 = None
-        waves2 = None
-        for i in id_s:
-            # print "i %s" % i
-            cursor = mongo.find("waves", {"sample_id": i[0], "chr": chromosome}, None, [("chr", 1)])
-            waves1 = common_utilities.CreateListFromCursor(cursor)
-        for i in id_r:
-            # print "i %s" % i
-            cursor = mongo.find("waves", {"sample_id": i[0], "chr": chromosome}, None, [("chr", 1)])
-            waves2 = common_utilities.CreateListFromCursor(cursor)
-            # print "waves1.length", len(waves1)
-            # for j in range(len(waves1)):
-            #   print waves1[j]
-        
-        
-        
-        
-        # figure out which peaks are same between the two samples.
-        both = []
 
-        i = 0
-        j = 0
-        max_i = len(waves1)
-        max_j = len(waves2)
-        # print "max_i: ", max_i
-        # print "max_j: ", max_j
-
-        while (i < max_i and j < max_j):
-            chromosome = waves1[i]['chr']
-            # print "DEBUG: Round 1 chromosome: ", chromosome
-            pos_i = waves1[i]['pos']
-            sdv_i = waves1[i]['stddev']
-            ht_i = waves1[i]['height']
-            jt = j - 1
-            while jt >= 0 and waves2[jt]['pos'] > (pos_i - 4 * sdv_i) :
-                # print "jt - waves1[i]", waves1[i]['pos'], waves1[i]['stddev'], waves2[jt]['pos'], waves2[jt]['stddev']
-                pvalue = stats.ks_test(pos_i, sdv_i, waves2[jt]['pos'], waves2[jt]['stddev'])
-                if (pvalue != 0):
-                    w = WavePair(chromosome, i, jt, pvalue, pos_i, waves2[jt]['pos'], sdv_i, waves2[jt]['stddev'], ht_i, waves2[jt]['height'])
-                    # if pvalue < 0.1:
-                    #    print_queue.put(w.to_string())
-                    both.append(w)
-                jt -= 1
-            while j < max_j and waves2[j]['pos'] < (pos_i + 4 * sdv_i):
-                # print "j  - waves1[i]", waves1[i]['pos'], waves1[i]['stddev'], waves2[j]['pos'], waves2[j]['stddev']
-                pvalue = stats.ks_test(pos_i, sdv_i, waves2[j]['pos'], waves2[j]['stddev'])
-                if (pvalue != 0):
-                    w = WavePair(chromosome, i, j, pvalue, pos_i, waves2[j]['pos'], sdv_i, waves2[j]['stddev'], ht_i, waves2[j]['height'])
-                    # if pvalue < 0.1:
-                    #    print_queue.put(w.to_string())
-                    both.append(w)
-                j += 1
-            i += 1
-            # print "i %s and max_i %s", i, max_i
-
-
-        # decide which peaks to use for normalization - make changes here.
-        for b in both:
-            ratio = float(b.ht1) / b.ht2
-            if b.p < 0.0005 and ratio > (float(1) / 20) and ratio < 20:
-                x.append(b.ht1)
-                y.append(b.ht2)
-
-
-    # h1 = histogram.Histogram("/home/afejes/temp/test_hist_x.svg", 100, x_max = 100)
-    # h1.add_data(x)
-    # h1.bin_data()
-    # h1.build()
-    # h1.save()
-
-    # h2 = histogram.Histogram("/home/afejes/temp/test_hist_y.svg", 100, x_max = 100)
-    # h2.add_data(y)
-    # h2.bin_data()
-    # h2.build()
-    # h2.save()
-
-    # NORMALIZATION
-    linear = odr.Model(f)
-    mydata = odr.Data(x, y)
-    myodr = odr.ODR(mydata, linear, [1])
-    myodr.set_job(fit_type = 2)    # do a linear model, then use that to figure out the non-linear model - initial first guess.
-
-    # myodr.set_iprint(final = 2)
-    fit = myodr.run()
-    coeff = fit.beta[::-1]
-    err = fit.sd_beta[::-1]
-    print "linear - least squares : coeff %s err %s" % (coeff, err)
-    myodr = odr.ODR(mydata, linear, coeff)
-    myodr.set_job(fit_type = 0)
-
-
-    fit2 = myodr.run()
-    # fit.pprint()
-    if fit2.stopreason[0] == 'Iteration limit reached':
-        print '(WWW) poly_lsq: Iteration limit reached, result not reliable!'
-    coeff = fit2.beta[::-1]
-    err = fit2.sd_beta[::-1]
-    print "ODR - explicit fit: coeff %s err %s" % (coeff, err)
-        # normalize the heights
-
-    # bp = scatterplot.ScatterPlot("/home/sbrown/temp/test.svg", 1200, 600)
-    # bp.add_and_zip_data(x, y)
-    # bp.add_regression(coeff)
-    # bp.build()
-    # bp.save()
-
-    # filter out from control
-    
-    '''
-    
-    
     # Collect all wavepairs that have a pair
     # Remove background peaks for each chromosome
     all_paired = []
@@ -311,9 +181,9 @@ def run(output, db):
             # print "i %s" % i
             cursor = mongo.find("waves", {"sample_id": i[0], "chr": chromosome}, None, [("pos", 1)])
             waves2 = common_utilities.CreateListFromCursor(cursor)
-        
+
         print "\nNow determining background levels for height of peaks on chromosome ", chromosome
-        bins = 70   # based on max peak height of 7
+        bins = 70    # based on max peak height of 7
         counts = [0] * bins
         thresh = [0] * bins
         for i in range(0, bins):
@@ -321,7 +191,7 @@ def run(output, db):
         for i in (waves1 + waves2):
             # combine h1 and h2 to determine background levels
             counts[int(i['height'] / 0.1)] += 1    # increment count where height1 is in bin of size 0.1
-            counts[int(i['height'] / 0.1)] += 1    # increment count where height2 is in bin of size 0.1
+            # counts[int(i['height'] / 0.1)] += 1    # increment count where height2 is in bin of size 0.1
         print "counts are: ", counts
         x = []
         y = []
@@ -335,14 +205,14 @@ def run(output, db):
         intercept = slr[1]
         print "slope: %s and intercept: %s for background peak height" % (slope, intercept)
         # find x-intercept, threshold for noise-signal
-        xint = abs(intercept/slope)
+        xint = abs(intercept / slope)
         print "height threshold between noise and signal is ", xint
         # REMOVE background peaks
         waves1[:] = [x for x in waves1 if x['height'] > xint]
         waves2[:] = [x for x in waves2 if x['height'] > xint]
-        
-        
-        
+
+
+
         # find pairs
         paired_data = []
         i = 0
@@ -394,7 +264,7 @@ def run(output, db):
                 all_paired.append(best)
 
             i += 1
-            
+
         # Now find unique peaks in waves2
         # find pairs
         i = 0
@@ -429,25 +299,10 @@ def run(output, db):
                 all_unpaired.append(w)
 
             j += 1
-            
-            
-
-        # SCOTT NOTE:
-            # why does this only write if ht1>ht2? Don't we want both?
-            # Also, shouldn't height normalization come into play when analysing differences in heights?
-
-        # print "The list of paired data for chromosome %s is %i:" % (chromosome, len(paired_data))
-        # for b in paired_data:
-        #    if (b.ht1 > b.ht2 * coeff):
-        #        print_queue.put(b.to_string())
-
-    # print unpaired data
-    # for i in all_unpaired:
-    #    print_queue.put(i.to_string())
 
 
     # Now determine FDR for every peak that has a pair, carry on with pairs that meet user cutoff.
-    user_ks_fdr = 0.05    # TODO: input by user
+    # user_ks_fdr = 0.05    # TODO: input by user
     bins = int(len(all_paired) * 0.05)    # bin size is 5% of total number of bins
     print "number of bins: ", bins
     counts = [0] * bins    # holds number of pairs in each bin
@@ -511,44 +366,44 @@ def run(output, db):
     # print paired data:
     # for i in all_paired:
     #    print_queue.put(i.to_string())
-    
+
     # determine fold change for paired peaks
     for i in all_paired:
         i.set_fc(math.log(i.ht1 / i.ht2, 2))
         # print peaks with significant enrichment/depletion
         # if(i.fc != 0):
         #    print_queue.put(i.to_string())
-    
+
     # determine "fold change" for unpaired peaks
     for i in all_unpaired:
         if i.ht1 == 0:    # unique control peak
             i.set_fc(math.log(1.0 / i.ht2, 2))
         else:    # unique sample peak
             i.set_fc(math.log(i.ht1 / 1.0, 2))
-    
+
     # now all entries in "all_paired" have fdr and fc, all entries in "all_unpaired" have fdr of -1, fc
-    
+
     # print everything, and significant results
     alldata = all_paired + all_unpaired
-    user_fc = 1.0
+    # user_fc = 1.0
     for i in alldata:
         print_queue.put(i.to_string())
-        
-        
+
+
     if print_thread is None or not print_thread.is_alive():
         pass
     else:
         while print_queue.qsize() > 0:
             print "waiting on print_queue to empty", print_queue.qsize()
             time.sleep(1)
-        print_thread.END_PROCESSES = True   
-        
-        
-    
+        print_thread.END_PROCESSES = True
+
+
+
     # print significant results to separate file
     print_queue2 = multiprocessing.Queue()
     # launch thread to read and process the print queue
-    print_thread2 = PrintThread.StringWriter(print_queue2, output, "result_nobg_2way.txt", True, True)
+    print_thread2 = PrintThread.StringWriter(print_queue2, output, "waves_comparison_significantwaves.txt", True, True)
 
     print "Now printing significant peaks to results file"
     for i in alldata:
@@ -585,7 +440,7 @@ def f(B, x):
 if __name__ == '__main__':
     if len(sys.argv) <= 3:
         print ("This program requires the name of the database config file, output path, and name of database.")
-        print" eg. python ImportWaveToDB.py /directory/database.conf directory/output/ yeast_epigenetics"
+        print" eg. python Compare_waves_sets_nobg_2way.py /directory/database.conf directory/output/ yeast_epigenetics"
         sys.exit()
     conf_file = sys.argv[1]
     output = sys.argv[2]
