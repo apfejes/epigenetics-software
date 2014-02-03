60fac12019e95e805159e41f43f299fb14db7124
Anthony Fejes <apfejes@kore.cmmt.ubc.ca>
2014-02-03 12:52:25 -0800

    Use MongoHost parameter in mongoengine connect config

--- Epigenetics/django_server/settings.py
+++ Epigenetics/django_server/settings.py
@@ -44,7 +44,7 @@ DATABASES = {
          # The following settings are not used with sqlite3:
          'USER': '',
          'PASSWORD': '',
-         'HOST': '',    # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+         'HOST': MONGO_HOST,    # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
  	'PORT': '',    # Set to empty string for default.
      }
  }
