6f557a87976cf7f6a5f4a8d330aa6b2400d09fab
Anthony Fejes <afejes@cmmt.ubc.ca>
2014-04-10 10:46:44 -0700

    New function to update sample meta data from a CSV file.

--- Epigenetics/Methylation_Data/UpdateSampleFromCSV.py
+++ Epigenetics/Methylation_Data/UpdateSampleFromCSV.py
@@ -19,24 +19,20 @@ import CommonUtils.Parameters as Parameters
 # from platform import system
 
 
-def importObjectsSampleData(mongo, csvbeta):
+def importObjectsSampleData(mongo, csvbeta, project_name, collection):
     if not os.path.isfile(csvbeta):
         print "Unable to find file %s" % csvbeta
         # wave_data_file = raw_input('wave file to load: ')
         sys.exit()
     first = True
     headers = {}
-    GSE = os.path.basename(csvbeta)
-    GSE = GSE[0:GSE.find("_")]
-    print "GSE in use: ", GSE
     count = 0
-    to_insert = []
     f = open(csvbeta, 'r')    # open file
     for b_line in f:
         count += 1
         if first:
             r = b_line.split(",")
-            headers = [h.replace('"', '').strip() for h in r[1:]]
+            headers = [h.replace('"', '').replace(".", "_").strip().lower() for h in r]
             print "all headers, processed:", headers
             first = False
             continue
@@ -44,36 +40,29 @@ def importObjectsSampleData(mongo, csvbeta):
         b = b_line.split(",")
         b = [bv.replace('"', '').strip() for bv in b]
         probe_data = {}
-        probe_data['pid'] = b[0]    # probe_name
-        probe_data['project'] = GSE
-        probe_data['b'] = {}
-        b = b[1:]
-        for i in range(0, len(headers)):
-            if b[i] != "NA":
-                probe_data['b'].update({headers[i]:float(b[i])})
-        to_insert.append(probe_data)
+        sampleid = None
+        for i, h in enumerate(headers):
+            if h == 'sampleid':
+                sampleid = b[i].replace(".", " ")
+            elif b[i]:
+                probe_data[h] = b[i]
 
+        if not sampleid:
+            print "No sampleid found in this dataset. There must be a field labeled as 'sampleid'"
+            sys.exit()
+        # print "sid = ", sampleid
+        # print "probe data = ", probe_data
+        # print "Updating: ", {"project":project_name, "sampleid":sampleid}
 
-        if len(to_insert) >= 50000:
-            rows = mongo.InsertBatchToDB("methylation", to_insert)
-            if rows == len(to_insert):
-                print "%i rows processed - batch successfully inserted" % count
-            else:
-                print "%i rows processed - batch insert failed!" % count
-            to_insert = []
-
-    rows = mongo.InsertBatchToDB("methylation", to_insert)
-    if rows == len(to_insert):
-        print "final batch successfully inserted - %i rows processed" % count
-    else:
-        print "final batch insert failed! - %i rows processed" % count
+        t = mongo.update(collection, {"project":project_name, "sampleid":sampleid}, {'$set':probe_data}, multiOpt = False)
+        # print t
     f.close()
 #===============================================================================
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("csvbeta", help = "The file name of the CSV beta file to import", type = str)
+    parser.add_argument("csvmetatdata", help = "The file name of the CSV beta file to import", type = str)
     parser.add_argument("-dbconfig", help = "An optional file to specify the database location - default is database.conf in MongoDB directory", type = str, default = None)
     parser.add_argument("-dbname", help = "name of the Database in the Mongo implementation to use - default is provided in the database.conf file specified", type = str, default = None)
     args = parser.parse_args()
@@ -82,6 +71,8 @@ if __name__ == "__main__":
         p.set("default_database", args.dbname)
     starttime = time.time()
     mongodb = Mongo_Connector.MongoConnector(p.get('server'), p.get('port'), p.get('default_database'))
-    importObjectsSampleData(mongodb, args.csvbeta)
+    collection = "samples"
+    project_name = raw_input('Enter the name of the project to insert in the ' + collection + ' collection of the ' + p.get('default_database') + ' database: ')
+    importObjectsSampleData(mongodb, args.csvmetatdata, project_name, collection)
     mongodb.close()
     print('Done in %s seconds') % int((time.time() - starttime))
