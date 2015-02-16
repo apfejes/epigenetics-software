118244628e8c449e9b0a131e4d9a6adde5be465d
afejes <afejes@omicia.com>
2015-02-16 13:37:00 -0800

    Cleaning up Nomenclature.py

--- Epigenetics/CommonUtils/Nomenclature.py
+++ Epigenetics/CommonUtils/Nomenclature.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on Aug 26, 2013
 
 @author: afejes
-'''
+"""
 
 # chromosome names for yeast.
 chr_yeast = {
@@ -27,24 +27,26 @@ chr_yeast = {
 
 @staticmethod
 def chromosome(database, chrom):
-    '''Function to convert the name of a chrom to the correct string for a given database'''
+    """Function to convert the name of a chrom to the correct string for
+    a given database"""
     lo = database.lowercase()
 
     if "human" in lo:
         if chrom.startswith("chr"):
             return chrom
         else:
-            return chr + str(chrom)
+            return "chr" + str(chrom)
     elif "yeast" in lo:
         if chrom.startswith("chr"):
             return chrom
         else:
-            return chr + chrom
-    elif "arabidopsis" in lo:    # Note: this is currently incorrect, arabidopsis doesn't normally have chr prefix.
+            return "chr" + chrom
+    elif "arabidopsis" in lo:   # Note: this is currently incorrect,
+                                # arabidopsis doesn't normally have chr prefix.
         if chrom.startswith("chr"):
             return chrom
         else:
-            return chr + chrom
+            return "chr" + chrom
     else:
         raise Exception("Unrecognized organism database type. Nomenclature.py")
 
