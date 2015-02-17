ea5f7029a0f66e5df69823e7071f5ca833e48579
Anthony Fejes <apfejes@gmail.com>
2015-02-16 20:46:32 -0800

    clean up Types

--- Epigenetics/CommonUtils/Types.py
+++ Epigenetics/CommonUtils/Types.py
@@ -1,19 +1,23 @@
-'''
+"""
 Created on Aug 26, 2013
 
 @author: afejes
-'''
+"""
+
 
 def is_int(s):
-    '''Test if element is an integer, by trying to convert to int. If it can't be done, it's not an int'''
+    """Test if element is an integer, by trying to convert to int.
+    If it can't be done, it's not an int"""
     try:
         int(s)
         return True
     except ValueError:
         return False
 
+
 def is_float(element):
-    '''Test if element is a float, by trying to convert to float. If it can't be done, it's not an float'''
+    """Test if element is a float, by trying to convert to float.
+    If it can't be done, it's not an float"""
     try:
         float(element)
     except ValueError:
@@ -22,8 +26,9 @@ def is_float(element):
 
 
 def is_bool(element):
-    '''Test if element is a boolean variable by checking if it is a string that says either true or false'''
-    if (element.lower() == "true" or element.lower() == "false"):
+    """Test if element is a boolean variable by checking if it
+    is a string that says either true or false"""
+    if element.lower() == "true" or element.lower() == "false":
         return True
     else:
         return False
